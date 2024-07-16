daf532d3eefd0041b68899f4c00a83414555fbb6
Harsh <hp92448@gmail.com>
2024-07-16 18:29:26 +0530

    form validation

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -2,34 +2,21 @@ import { motion } from "framer-motion";
 import { BsArrowRight } from "react-icons/bs";
 
 import { fadeIn } from "../../variants";
-import { useState } from "react";
+import { Formik, Field, Form } from "formik";
+import { validation } from "./validation.jsx";
 
 const Contact = () => {
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setIsLoading(true);
-
-    const myForm = event.target;
-    const formData = new FormData(myForm);
-
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => alert("Thank you. I will get back to you ASAP."))
-      .catch((error) => console.log(error))
-      .finally(() => setIsLoading(false));
+  const initialValues = {
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
   };
 
   return (
     <div className="h-full bg-primary/100">
       <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
-        {/* text & form */}
         <div className="flex flex-col w-full max-w-[700px]">
-          {/* text */}
           <motion.h2
             variants={fadeIn("up", 0.2)}
             initial="hidden"
@@ -39,78 +26,97 @@ const Contact = () => {
           >
             Let's <span className="text-accent">connect.</span>
           </motion.h2>
-
-          {/* form */}
-          <motion.form
+          <motion.div
             variants={fadeIn("up", 0.4)}
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="flex-1 flex flex-col gap-6 w-full mx-auto"
-            onSubmit={handleSubmit}
-            autoComplete="off"
-            autoCapitalize="off"
-            // only needed for production (in netlify) to accept form input
-            data-netlify="true"
           >
-            {/* input group */}
-            <div className="flex gap-x-6 w-full">
-              <input
-                type="text"
-                name="name"
-                placeholder="Name"
-                className="input"
-                disabled={isLoading}
-                aria-disabled={isLoading}
-                required
-                aria-required
-              />
-              <input
-                type="email"
-                name="email"
-                placeholder="E-mail"
-                className="input"
-                disabled={isLoading}
-                aria-disabled={isLoading}
-                required
-                aria-required
-              />
-            </div>
-            <input
-              type="text"
-              name="subject"
-              placeholder="Subject"
-              className="input"
-              disabled={isLoading}
-              aria-disabled={isLoading}
-              required
-              aria-required
-            />
-            <textarea
-              name="message"
-              placeholder="Message..."
-              className="textarea"
-              disabled={isLoading}
-              aria-disabled={isLoading}
-              required
-              aria-required
-            />
-            <button
-              type="submit"
-              className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group"
-              disabled={isLoading}
-              aria-disabled={isLoading}
-            >
-              <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
-                Let's talk
-              </span>
+            <Formik initialValues={initialValues} validationSchema={validation}>
+              {({ errors }) => (
+                <Form className="flex-1 flex flex-col gap-6 w-full mx-auto">
+                  <div className="grid grid-cols-2 gap-4 w-full">
+                    <div className="flex flex-col gap-1">
+                      <Field
+                        className={`input ${
+                          errors.name ? "border-red-600" : ""
+                        }`}
+                        type="text"
+                        name="name"
+                        placeholder="Enter Full Name"
+                      />
+                      {errors.name && (
+                        <span className="text-red-600 capitalize">
+                          {errors.name}
+                        </span>
+                      )}
+                    </div>
+                    <div className="flex flex-col gap-1">
+                      <Field
+                        className={`input ${
+                          errors.email ? "border-red-600" : ""
+                        }`}
+                        type="email"
+                        name="email"
+                        placeholder="Enter E-mail"
+                      />
+                      {errors.email && (
+                        <span className="text-red-600 capitalize">
+                          {errors.email}
+                        </span>
+                      )}
+                    </div>
+                    <div className="flex flex-col gap-1 col-span-2">
+                      <Field
+                        className={`input ${
+                          errors.subject ? "border-red-600" : ""
+                        }`}
+                        type="text"
+                        name="subject"
+                        placeholder="Enter a Subject"
+                      />
+                      {errors.subject && (
+                        <span className="text-red-600 capitalize">
+                          {errors.subject}
+                        </span>
+                      )}
+                    </div>
+                    <div className="flex flex-col gap-1 col-span-2">
+                      <Field
+                        component="textarea"
+                        className={`textarea ${
+                          errors.message ? "border-red-600" : ""
+                        }`}
+                        type="textarea"
+                        name="message"
+                        placeholder="Enter a message"
+                      />
+                      {errors.message && (
+                        <span className="text-red-600 capitalize">
+                          {errors.message}
+                        </span>
+                      )}
+                    </div>
+                    <div className="col-span-2">
+                      <button
+                        type="submit"
+                        className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group"
+                      >
+                        <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
+                          Let's talk
+                        </span>
 
-              <BsArrowRight
-                className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]"
-                aria-hidden
-              />
-            </button>
-          </motion.form>
+                        <BsArrowRight
+                          className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]"
+                          aria-hidden
+                        />
+                      </button>
+                    </div>
+                  </div>
+                </Form>
+              )}
+            </Formik>
+          </motion.div>
         </div>
       </div>
     </div>
